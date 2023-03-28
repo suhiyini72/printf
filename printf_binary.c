e2334d0c15239d6093f2e992c7e4f26a5abf29a8
Patience123D <122967192+Patience123D@users.noreply.github.com>
2023-03-28 23:42:05 +0000

    Update printf_binary.c

--- printf_binary.c
+++ printf_binary.c
@@ -3,11 +3,11 @@
 /**
  * printf_binary - prints a binary number
  * @num: number arguements
- * @printed: the printed characters
+ * @print: printed characters
  * Return: printed charcaters
  */
 
-int printf_binary(unsigned int num, int printed)
+int printf_binary(unsigned int num, int print)
 {
 	int binary[32] = {0};
 	int i = 0;
@@ -15,8 +15,8 @@ int printf_binary(unsigned int num, int printed)
 	if (num == 0)
 	{
 		_putchar('0');
-		printed++;
-		return (printed);
+		print++;
+		return (print);
 	}
 
 	while (num > 0)
@@ -30,7 +30,7 @@ int printf_binary(unsigned int num, int printed)
 	{
 		i--;
 		_putchar('0' + binary[i]);
-		printed++;
+		print++;
 	}
 
 	return (printed);
