936afb30ad1e58c20de0009e4f606a08a161ade0
Patience123D <122967192+Patience123D@users.noreply.github.com>
2023-03-28 23:43:11 +0000

    Update printf_char.c

--- printf_char.c
+++ printf_char.c
@@ -2,15 +2,15 @@
 
 /**
  * printf_char - prints a binary number
- * @args: the arguements to use
- * @printed: the printed characters
+ * @args: arguements to use
+ * @print: the printed characters
  * Return: printed charcaters
  */
 
-int printf_char(va_list args, int printed)
+int printf_char(va_list args, int print)
 {
 	int character = va_arg(args, int);
 
 	_putchar(character);
-	return (printed + 1);
+	return (print + 1);
 }
