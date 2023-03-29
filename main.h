a8d07cc5e6551c58792a515d63910949afc97205
Patience123D <122967192+Patience123D@users.noreply.github.com>
2023-03-29 00:01:56 +0000

    Update main.h

--- main.h
+++ main.h
@@ -7,16 +7,16 @@
 #include <limits.h>
 
 int _putchar(char character);
-int printf_char(va_list args, int printed);
-int printf_string(va_list args, int printed);
-int printf_integer(va_list args, int printed);
-int selector(const char *format, va_list args, int printed);
+int printf_char(va_list args, int print);
+int printf_string(va_list args, int print);
+int printf_integer(va_list args, int print);
+int selector(const char *format, va_list args, int print);
 int printf_binary(unsigned int num, int printed);
 int _printf(const char *format, ...);
-int _x(unsigned int num, int printed, int uppercase);
-int printf_octal(unsigned int num, int printed);
-int printf_unsigned(unsigned int num, int printed);
-int printf_reverse(va_list args, int printed);
-int printf_pointer(va_list args, int printed);
+int _x(unsigned int num, int print, int uppercase);
+int printf_octal(unsigned int num, int print);
+int printf_unsigned(unsigned int num, int print);
+int printf_reverse(va_list args, int print);
+int printf_pointer(va_list args, int print);
 
 #endif
